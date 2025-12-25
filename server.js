c7dd11e8af700dea6e3700a924c4436e4647594a
Aditya <adityaa00098@gmail.com>
2025-12-25 12:42:11 +0530

    testing done

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+const emailRoute = require("./routes/emailRoutes");
+app.use("/", emailRoute);
+
 
 
 app.use(express.json());
 
-app.get("/". req,res => {
-    res.send("This is home route");
-})
+
+
 
 app.listen(3000,() => {
     console.log("Server is running on port 3000");
